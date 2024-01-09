d0625e9dc1320eac9342af35602a767ce604b2c9
WestspringIT-Implementation <156073641+WestspringIT-Implementation@users.noreply.github.com>
2024-01-09 14:36:40 +0000

    Update Remove-DotNET_Componants.ps1

--- DotNET_Clean-Uninstall/Remove-DotNET_Componants.ps1
+++ DotNET_Clean-Uninstall/Remove-DotNET_Componants.ps1
@@ -1,1 +1,19 @@
-##boilerplate
+# Initialise Vars
+$Uri = "https://github.com/WestspringIT-Implementation/Internal_scripts/raw/main/DotNET_Clean-Uninstall/dotnet-core-uninstall-1.6.0.msi"0
+$FilePath =  "C:\IT\dotnet-core-uninstall\"
+$MSIName = "1.6.0.msi"
+$MSIPath = "$FilePath$MSIName"
+
+# Create detination directory if it doesn't already exist
+If (!(Test-Path $FilePath)) {
+    New-Item -ItemType Directory -Path $FilePath -Force
+}
+
+# Download the donnet-core-uninstall .msi
+Invoke-WebRequest -Uri $Uri -OutFile $MSIPath
+
+# Install donnet-core-uninstall .msi
+msiexec.exe /i $MSIPath /quiet /norestart
+
+# Clean up
+Remove-Item -Path $FilePath -Recurse -ErrorAction SilentlyContinue
