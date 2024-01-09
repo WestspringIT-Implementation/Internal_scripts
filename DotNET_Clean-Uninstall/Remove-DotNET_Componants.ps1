2a04115cf8bc479f1e54d28e899d6d4e3c939fd9
WestspringIT-Implementation <156073641+WestspringIT-Implementation@users.noreply.github.com>
2024-01-09 14:38:37 +0000

    Update Remove-DotNET_Componants.ps1

--- DotNET_Clean-Uninstall/Remove-DotNET_Componants.ps1
+++ DotNET_Clean-Uninstall/Remove-DotNET_Componants.ps1
@@ -1,5 +1,5 @@
 # Initialise Vars
-$Uri = "https://github.com/WestspringIT-Implementation/Internal_scripts/raw/main/DotNET_Clean-Uninstall/dotnet-core-uninstall-1.6.0.msi"0
+$Uri = "https://github.com/WestspringIT-Implementation/Internal_scripts/raw/main/DotNET_Clean-Uninstall/dotnet-core-uninstall-1.6.0.msi"
 $FilePath =  "C:\IT\dotnet-core-uninstall\"
 $MSIName = "1.6.0.msi"
 $MSIPath = "$FilePath$MSIName"
